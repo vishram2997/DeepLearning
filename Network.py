5fca0a9bb41bdc75408872164f601d83cdc9f498
Vishram Singh <vissingh@tesla.com>
2019-01-26 20:21:46 -0800

    added 4 layer

--- Network.py
+++ Network.py
@@ -13,11 +13,12 @@ n_input = 784   # input layer (28x28 pixels)
 n_hidden1 = 512 # 1st hidden layer
 n_hidden2 = 256 # 2nd hidden layer
 n_hidden3 = 128 # 3rd hidden layer
+n_hidden4 = 64 # 3rd hidden layer
 n_output = 10   # output layer (0-9 digits)
 
 
 learning_rate = 1e-4
-n_iterations = 10000
+n_iterations = 2000
 batch_size = 128
 dropout = 0.5
 
@@ -30,21 +31,24 @@ weights = {
     'w1': tf.Variable(tf.truncated_normal([n_input, n_hidden1], stddev=0.1)),
     'w2': tf.Variable(tf.truncated_normal([n_hidden1, n_hidden2], stddev=0.1)),
     'w3': tf.Variable(tf.truncated_normal([n_hidden2, n_hidden3], stddev=0.1)),
-    'out': tf.Variable(tf.truncated_normal([n_hidden3, n_output], stddev=0.1)),
+    'w4': tf.Variable(tf.truncated_normal([n_hidden3, n_hidden4], stddev=0.1)),
+    'out': tf.Variable(tf.truncated_normal([n_hidden4, n_output], stddev=0.1)),
 }
 
 biases = {
     'b1': tf.Variable(tf.constant(0.1, shape=[n_hidden1])),
     'b2': tf.Variable(tf.constant(0.1, shape=[n_hidden2])),
     'b3': tf.Variable(tf.constant(0.1, shape=[n_hidden3])),
+    'b4': tf.Variable(tf.constant(0.1, shape=[n_hidden4])),
     'out': tf.Variable(tf.constant(0.1, shape=[n_output]))
 }
 
 layer_1 = tf.add(tf.matmul(X, weights['w1']), biases['b1'])
 layer_2 = tf.add(tf.matmul(layer_1, weights['w2']), biases['b2'])
 layer_3 = tf.add(tf.matmul(layer_2, weights['w3']), biases['b3'])
-layer_drop = tf.nn.dropout(layer_3, keep_prob)
-output_layer = tf.matmul(layer_3, weights['out']) + biases['out']
+layer_4 = tf.add(tf.matmul(layer_3, weights['w4']), biases['b4'])
+layer_drop = tf.nn.dropout(layer_4, keep_prob)
+output_layer = tf.matmul(layer_4, weights['out']) + biases['out']
 
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=output_layer))
