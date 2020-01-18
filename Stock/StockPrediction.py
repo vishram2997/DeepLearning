67de41bc4e401ab9719ab3aa47d0fb3f2de0cd23
Vishram Singh <vissingh@tesla.com>
2020-01-17 16:11:03 -0800

    StockPrediction

--- Stock/StockPrediction.py
+++ Stock/StockPrediction.py
@@ -4,6 +4,8 @@ import numpy as np
 import os
 import csv
 import time
+import matplotlib.pyplot as plt
+
 
 apikey = "3HJSYM7HFM6OT87F"
 URL = "https://www.alphavantage.co/query?function="
@@ -30,6 +32,221 @@ def downloadDataFile():
 
 
 
+import matplotlib.dates as mdates
+
+years = mdates.YearLocator()   # every year
+months = mdates.MonthLocator()  # every month
+years_fmt = mdates.DateFormatter('%Y')
 
 
+df = pd.read_csv("TSLA.csv")
+
+# First calculate the mid prices from the highest and lowest
+high_prices = df.loc[:,'high'].as_matrix()
+low_prices = df.loc[:,'low'].as_matrix()
+mid_prices = (high_prices+low_prices)/2.0
+train_data = mid_prices[:1200]
+test_data = mid_prices[1200:]
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+train_data = train_data.reshape(-1,1)
+test_data = test_data.reshape(-1,1)
+
+print(train_data.shape)
+# Train the Scaler with training data and smooth data
+smoothing_window_size = 200
+for di in range(0,1000,smoothing_window_size):
+    scaler.fit(train_data[di:di+smoothing_window_size,:])
+    train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
+
+# You normalize the last bit of remaining data
+scaler.fit(train_data[di+smoothing_window_size:,:])
+train_data[di+smoothing_window_size:,:] = scaler.transform(train_data[di+smoothing_window_size:,:])
+
+# Reshape both train and test data
+train_data = train_data.reshape(-1)
+
+# Normalize test data
+test_data = scaler.transform(test_data).reshape(-1)
+
+EMA = 0.0
+gamma = 0.1
+for ti in range(1200):
+  EMA = gamma*train_data[ti] + (1-gamma)*EMA
+  train_data[ti] = EMA
+
+# Used for visualization and test purposes
+all_mid_data = np.concatenate([train_data,test_data],axis=0)
+import datetime as dt
+def standardAverage():
+    window_size = 10
+    N = train_data.size
+    std_avg_predictions = []
+    std_avg_x = []
+    mse_errors = []
+
+    
+    for pred_idx in range(window_size,N):
+
+        if pred_idx >= N:
+            date = dt.datetime.strptime(k, '%Y-%m-%d').date() + dt.timedelta(days=1)
+        else:
+            date = df.loc[pred_idx,'timestamp']
+
+        std_avg_predictions.append(np.mean(train_data[pred_idx-window_size:pred_idx]))
+        mse_errors.append((std_avg_predictions[-1]-train_data[pred_idx])**2)
+        std_avg_x.append(date)
+        
+        
+    plt.figure(figsize = (18,9))
+    plt.plot(range(df.shape[0]),all_mid_data,color='b',label='True')
+    plt.plot(range(window_size,N),std_avg_predictions,color='orange',label='Prediction')
+    #plt.xticks(range(0,df.shape[0],50),df['Date'].loc[::50],rotation=45)
+    plt.xlabel('Date')
+    plt.ylabel('Mid Price')
+    plt.legend(fontsize=18)
+    plt.show()
+    
+    
+def expAverage():
+    window_size = 100
+    N = train_data.size
+
+    run_avg_predictions = []
+    run_avg_x = []
+
+    mse_errors = []
+
+    running_mean = 0.0
+    run_avg_predictions.append(running_mean)
+
+    decay = 0.5
+
+    for pred_idx in range(1,N):
+        if pred_idx >= N:
+                date = dt.datetime.strptime(k, '%Y-%m-%d').date() + dt.timedelta(days=1)
+        else:
+            date = df.loc[pred_idx,'timestamp']
+            
+        running_mean = running_mean*decay + (1.0-decay)*train_data[pred_idx-1]
+        run_avg_predictions.append(running_mean)
+        mse_errors.append((run_avg_predictions[-1]-train_data[pred_idx])**2)
+        run_avg_x.append(date)
+
+    print('MSE error for EMA averaging: %.5f'%(0.5*np.mean(mse_errors)))
+    plt.figure(figsize = (18,9))
+    plt.plot(range(df.shape[0]),all_mid_data,color='b',label='True')
+    plt.plot(range(0,N),run_avg_predictions,color='orange', label='Prediction')
+    #plt.xticks(range(0,df.shape[0],50),df['Date'].loc[::50],rotation=45)
+    plt.xlabel('Date')
+    plt.ylabel('Mid Price')
+    plt.legend(fontsize=18)
+    plt.show()
+    
+    
+
+from sklearn.model_selection import train_test_split
+df['Date'] = df['timestamp'].astype('datetime64[ns]')
+df =  df.sort_values(by='Date')
+
+
+
+
+train_cols = ["open","high","low","close","volume"]
+df_train, df_test = train_test_split(df, train_size=0.8, test_size=0.2, shuffle=False)
+print("Train and Test size", len(df_train), len(df_test))
+# scale the feature MinMax, build array
+x = df_train.loc[:,train_cols].values
+min_max_scaler = MinMaxScaler()
+x_train = min_max_scaler.fit_transform(x)
+x_test = min_max_scaler.transform(df_test.loc[:,train_cols])
+
+TIME_STEPS = 60
+from tqdm import tqdm_notebook
  
+def build_timeseries(mat, y_col_index):
+    # y_col_index is the index of column that would act as output column
+    # total number of time-series samples would be len(mat) - TIME_STEPS
+    dim_0 = mat.shape[0] - TIME_STEPS
+    dim_1 = mat.shape[1]
+    x = np.zeros((dim_0, TIME_STEPS, dim_1))
+    y = np.zeros((dim_0,dim_1))
+    
+    for i in tqdm_notebook(range(dim_0)):
+        x[i] = mat[i:TIME_STEPS+i]
+        y[i] = mat[TIME_STEPS+i]
+    print("length of time-series i/o",x.shape,y.shape)
+    return x, y
+
+
+def trim_dataset(mat, batch_size):
+    """
+    trims dataset to a size that's divisible by BATCH_SIZE
+    """
+    no_of_rows_drop = mat.shape[0]%batch_size
+    if(no_of_rows_drop > 0):
+        return mat[:-no_of_rows_drop]
+    else:
+        return mat
+
+BATCH_SIZE = 60
+    
+x_t, y_t = build_timeseries(x_train, 3)
+x_t = trim_dataset(x_t, BATCH_SIZE)
+y_t = trim_dataset(y_t, BATCH_SIZE)
+x_temp, y_temp = build_timeseries(x_test, 3)
+x_val, x_test_t = np.split(trim_dataset(x_temp, BATCH_SIZE),2)
+y_val, y_test_t = np.split(trim_dataset(y_temp, BATCH_SIZE),2)
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras import optimizers
+from keras.callbacks import CSVLogger
+
+
+OUTPUT_PATH = "./model/"
+your_epochs = 60
+
+lstm_model = Sequential()
+
+lstm_model.add(LSTM(100, batch_input_shape=(BATCH_SIZE, TIME_STEPS, x_t.shape[2]), dropout=0.0, recurrent_dropout=0.0, stateful=True,     kernel_initializer='random_uniform'))
+lstm_model.add(Dropout(0.2))
+
+
+
+lstm_model.add(Dense(20,activation='relu'))
+lstm_model.add(Dense(units=1,activation='sigmoid'))
+
+
+optimizer = optimizers.RMSprop(lr=0.001)
+lstm_model.compile(loss='mean_squared_error', optimizer=optimizer)
+
+
+csv_logger = CSVLogger(os.path.join(OUTPUT_PATH, 'your_log_name' + '.log'), append=True)
+
+
+history = lstm_model.fit(x_t, y_t, epochs=your_epochs, verbose=2, batch_size=60,
+                    shuffle=False, validation_data=(trim_dataset(x_val, BATCH_SIZE),
+                    trim_dataset(y_val, BATCH_SIZE)), callbacks=[csv_logger])
+
+
+lstm_model.save("lstm_model.hdf5")
+
+#lstm_model.load_weights("lstm_model.hdf5")
+
+#real_stock_price = df.iloc[:, 1:2].values
+predictPrice = lstm_model.predict(x_val[150:],batch_size=60)
+
+plt.plot(predictPrice, color = 'green', label = 'Predicted TATA Stock Price')
+plt.title('TESLA Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('TESLA Stock Price')
+plt.legend()
+plt.show()
+
+
+
